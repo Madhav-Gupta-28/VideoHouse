4027f6c61028149df2e040615417d25dc4e0e859
Madhav <guptamadhav2828@gmail.com>
2023-06-08 22:06:39 +0530

    made some changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { ColorModeScript, ChakraProvider, theme  } from '@chakra-ui/react';
+
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
